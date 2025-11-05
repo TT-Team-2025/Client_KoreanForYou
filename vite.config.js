e26ed1ed81cf4f740b440cb7927bd52427e5a2e8
1jsjs <pjs020201@naver.com>
2025-11-05 16:27:29 +0900

    chore(#1): setup axiosInstance and Vite proxy for API connection without .env file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // 🔹 FastAPI 서버 주소
+        changeOrigin: true,              // 🔹 Origin 헤더 맞춰주기
+        secure: false,                   // 🔹 HTTPS가 아닐 경우 false
+      },
+    },
+  },
 })
