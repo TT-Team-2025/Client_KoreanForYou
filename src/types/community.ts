bca63b3c4c5786edce7e075750695b5f517e9517
Xuswnss <cooconut0226@gmail.com>
2025-11-06 15:04:21 +0900

    fix(#9) postCategory type 추가

--- src/types/community.ts
+++ src/types/community.ts
@@ -1,11 +1,18 @@
 import { BaseResponse, DateString } from "./commonTypes";
 
+export enum PostCategory {
+  QNA = "Q&A",
+  INFO_SHARE = "정보공유",
+  FREE = "자유게시판",
+  JOB_INFO = "취업정보",
+}
+
 export interface Post {
   post_id: number;
   user_id: number;
   title: string;
   content: string;
-  category: string;
+  category: PostCategory;
   view_count: number;
   created_at: DateString;
   updated_at: DateString;
